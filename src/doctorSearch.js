9c4aa00cef51c598c7727c14e1ae239ea0161c78
Scott Bergler <commandinghands@gmail.com>
2019-01-18 10:54:25 -0800

    add an API url with template variables and start the UI in index.html.

--- src/doctorSearch.js
+++ src/doctorSearch.js
@@ -3,7 +3,7 @@ class DoctorSearch {
   getDoctors(malady) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${malady}&user_location=45.5122%2C-122.6587%2C100&skip=0&limit=10&user_key=0a6052a8fe07881c0f08c326c929f63d`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${malady}&user_location=45.5122%2C-122.6587%2C100&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
