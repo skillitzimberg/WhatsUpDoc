288f974d5158c57997917bcf868c221a727ad6d2
Scott Bergler <commandinghands@gmail.com>
2019-01-18 18:16:42 -0800

    edit api url to fix an error in searching first_name.

--- src/doctorSearch.js
+++ src/doctorSearch.js
@@ -3,7 +3,7 @@ class DoctorSearch {
   getDoctors(malady, first_name, last_name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name${first_name}&last_name=${last_name}&query=${malady}&location=45.5122%2C-122.6587%2C100&user_location=45.5122%2C-122.6587&sort=last-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${first_name}&last_name=${last_name}&query=${malady}&location=45.5122%2C-122.6587%2C100&user_location=45.5122%2C-122.6587&sort=last-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
